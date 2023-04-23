6a4b8a6141bb14d353f1b6781341453e836a76da
Jeb Nicholson <26765018+jeb5@users.noreply.github.com>
2023-04-23 18:14:12 +1200

    Fixed tv show title alignment bug

---
+++ components/elements/Loading.tsx
@@ -0,0 +1,6 @@
+import LoadingSVG from "../../assets/icons/loading.svg";
+import styles from "./Loading.module.css";
+
+export default function Loading() {
+	return <LoadingSVG className={styles.loadingSVG} />;
+}
--- components/home_search/HomeSearchResults.tsx
+++ components/home_search/HomeSearchResults.tsx
@@ -10,32 +10,36 @@ export default function HomeSearchResults({ shows }: { shows: ShowBrief[] }) {
 	const [topShow, ...restShows] = shows;
 	return (
 		<div className={styles.searchCarousel}>
-			<div className={cn(styles.show, styles.topResultShow)} key={topShow.url}>
-				<a className={styles.show} href={topShow.url}>
-					<Image
-						src={topShow.posterURL}
-						alt=""
-						className={`${styles.poster} showPoster`}
-						width={TOP_RESULT_POSTER_WIDTH}
-						height={TOP_RESULT_POSTER_WIDTH * (3 / 2)}
-					/>
-					<div className={styles.showName}>{topShow.name}</div>
-					<div className={styles.showYear}>{topShow.year}</div>
-				</a>
-			</div>
-			<div className={styles.restOfShows}>
-				{restShows.map((show, index) => {
+			<div className={styles.shows}>
+				<div className={cn(styles.show, styles.topResultShow)} key={topShow.url}>
+					<a className={styles.show} href={topShow.url}>
+						<Image
+							src={topShow.posterURL}
+							alt=""
+							className={"showPoster"}
+							width={TOP_RESULT_POSTER_WIDTH}
+							height={TOP_RESULT_POSTER_WIDTH * (3 / 2)}
+						/>
+						<div className={styles.showDetails}>
+							<div className={styles.showName}>{topShow.name}</div>
+							<div className={styles.showYear}>{topShow.year}</div>
+						</div>
+					</a>
+				</div>
+				{restShows.map(show => {
 					return (
 						<div className={styles.show} key={show.url}>
 							<a className={styles.show} href={show.url}>
 								<Image
 									src={show.posterURL}
 									alt=""
-									className={`${styles.poster} showPoster`}
+									className={"showPoster"}
 									width={POSTER_WIDTH}
 									height={POSTER_WIDTH * (3 / 2)}
 								/>
-								<div className={styles.showName}>{show.name}</div>
+								<div className={styles.showDetails}>
+									<div className={styles.showName}>{show.name}</div>
+								</div>
 							</a>
 						</div>
 					);
